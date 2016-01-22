3881dfaa17ecb8ade85ca08ce6ea9584743fd526
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-01-22 19:56:33 +0100

    Add Sentence model and PrologManager

---
+++ src/kck/helpers/PrologManager.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kck.helpers;
+
+import kck.models.Sentence;
+/**
+ *
+ * @author arkus
+ */
+public class PrologManager {
+    private final String MOVE = "move(";
+    private final String DIR = "dir(";
+    private final String GOAL = "goal(";
+    private final String BRACKET_END = ")";
+    
+    public Sentence getResult(String sentence) { 
+        String[] words = sentence.split(" ");
+        String query = "zdanie(X, [" + String.join(",", words) + "], [])";
+        // TODO: add JPL to get this code working
+//        if(Query.hasSolution(query)) {
+//            String queryResult = Query.oneSolution().get("X");
+//            return new Sentence(getMove(queryResult), getDirection(queryResult), getGoal(queryResult), getApproach(queryResult));
+//        }
+        return new Sentence(); // Delete this
+    }
+    
+    protected String getMove(String sentence) {
+        if(sentence.contains(MOVE)) {
+            int moveStart = sentence.indexOf(MOVE) + MOVE.length();
+            int moveEnd = sentence.indexOf(BRACKET_END, moveStart);
+            return sentence.substring(moveStart, moveEnd);
+        }
+        return "";
+    }
+    
+    protected String getDirection(String sentence) {
+        if(sentence.contains(DIR)) {
+            int dirStart = sentence.indexOf(DIR) + DIR.length();
+            int dirEnd = sentence.indexOf(BRACKET_END, dirStart);
+            return sentence.substring(dirStart, dirEnd);
+        }
+        return "";
+    }
+    
+    protected String getGoal(String sentence) {
+        if(sentence.contains(GOAL)) {
+            int goalStart = sentence.indexOf(GOAL) + GOAL.length();
+            int goalEnd = sentence.indexOf(BRACKET_END, goalStart);
+            return sentence.substring(goalStart, goalEnd);
+        }
+        return "";
+    }
+
+    protected String getApproach(String sentence) {
+        //TODO: get approach from sentence
+        return "";
+    }
+}
---
+++ src/kck/models/Sentence.java
@@ -0,0 +1,45 @@
+package kck.models;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arkus
+ */
+public class Sentence {
+    protected String move; 
+    protected String goal;
+    protected String direction;
+    protected String approach;
+
+    public Sentence() {
+    }
+
+    public Sentence(String move, String goal, String direction, String approach) {
+        this.move = move;
+        this.goal = goal;
+        this.direction = direction;
+        this.approach = approach;
+    }
+
+    public String getApproach() {
+        return approach;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getMove() {
+        return move;
+    }
+    
+}
