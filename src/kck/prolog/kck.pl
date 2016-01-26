9c8bba4d9b6a94eb155b62037d2b7f65ea8b648e
Arkadiusz Żmudzin <arkus7@users.noreply.github.com>
2016-01-26 23:08:37 +0100

    Add sentence with move, goal, direction and approach. Update approaches.

--- src/kck/prolog/kck.pl
+++ src/kck/prolog/kck.pl
@@ -4,6 +4,10 @@ zdanie(SEM) --> sposob(Y),czas(X), {SEM=move(Y,X), verify(SEM) }.
 zdanie(move(X,Y,Z)) --> czas(X), sposob(Z), kier(Y), {SEM=move(X,Z,Y), verify(SEM) }.
 zdanie(move(X,Y,Z)) --> czas(X), kier(Y), sposob(Z), {SEM=move(X,Y,Z), verify(SEM) }.
 zdanie(move(X,Y,Z)) --> kier(Y), sposob(Z),  czas(X), {SEM=move(Y,Z,X), verify(SEM) }.
+zdanie(move(X,Y,Z,A)) --> czas(X), kier(Y,A), sposob(Z).
+zdanie(move(X,Y,Z,A)) --> czas(X), sposob(Z), kier(Y,A).
+zdanie(move(X,Y,Z,A)) --> kier(Y,A), sposob(Z), czas(X).
+zdanie(move(X,Y,Z,A)) --> sposob(Z), czas(X), kier(Y,A).
 
 czas(walk) --> [idz].
 czas(turn) --> [skrec].
@@ -17,6 +21,8 @@ przyim(cel) --> [ku].
 przyim(dop) --> [do].
 
 kier(goal(X)) --> przyim(P), cel(P,X).
+kier(goal(X), Y) --> przyim(P), cel(P, X), kier(Y), {Y=dir(_)}.
+kier(goal(X), Y) --> kier(Y), przyim(P), cel(P,X), {Y=dir(_)}.
 
 kier(dir(left)) --> [w, lewo].
 kier(dir(right)) --> [w, prawo].
@@ -32,9 +38,14 @@ kier(dir(nw)) --> [na, polnocny, zachod].
 
 sposob(app(straight)) --> [prosto].
 sposob(app(sl)) --> [lewym, lagodnym, lukiem].
+sposob(app(sl)) --> [lagodnym, lewym, lukiem].
 sposob(app(sr)) --> [prawym, lagodnym, lukiem].
+sposob(app(sr)) --> [lagodnym, prawym, lukiem].
 sposob(app(shr)) --> [prawym, ostrym, lukiem].
+sposob(app(shr)) --> [ostrym, prawym, lukiem].
 sposob(app(shl)) --> [lewym, ostrym, lukiem].
+sposob(app(shl)) --> [ostrym, lewym, lukiem].
+
 
 cel(dop, lamp) --> [lampy].
 cel(dop, lake) --> [jeziora].
@@ -50,10 +61,9 @@ cel(dop, river) --> [rzeki].
 cel(dop, busstop) --> [przystanku].
 cel(dop, monument) --> [pomnika].
 cel(dop, mountain) --> [gory].
-cel(dop, car) --> [samochod].
-cel(dop, tracks) --> [tory].
-cel(dop, graveyard) --> [cmentarz].
-
+cel(dop, car) --> [samochodu].
+cel(dop, tracks) --> [torow].
+cel(dop, graveyard) --> [cmentarza].
 
 cel(cel, light) --> [lampie].
 cel(cel, lake) --> [jezioru].
@@ -73,8 +83,7 @@ cel(cel, car) --> [samochodowi].
 cel(cel, tracks) --> [torom].
 cel(cel, graveyard) --> [cmantarzowi].
 
-
-
+% trzeba ogarnąć te verify, tak, żeby było wiadomo co gdzie i jak
 
 verify(move(walk,_)).
 verify(move(_, walk)).
@@ -85,32 +94,3 @@ verify(move(turn,X)) :- X=sl ; X=sr; X=shr; X=shl.
 verify(move(turn,X,_)) :- X=sl ; X=sr; X=shr; X=shl.
 verify(move(turn,_,X)) :- X=sl ; X=sr; X=shr; X=shl.
 verify(move(_,X,turn)) :- X=sl ; X=sr; X=shr; X=shl.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
