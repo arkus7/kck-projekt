5820c77fd535117ec6041fce08f583c31d8cad32
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-04-11 22:00:00 +0200

    Remove synchronize prolog files

--- src/kck/prolog/PrologManager.java
+++ src/kck/prolog/PrologManager.java
@@ -42,7 +42,7 @@ public class PrologManager {
 
     public PrologManager() {
         initJPL();
-        checkAndUploadFile();
+//        checkAndUploadFile();
     }
     
     public String getLocalizedGoal(String goalName, WordCase wordCase) {
